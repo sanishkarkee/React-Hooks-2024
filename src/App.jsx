acf13f58e29356f4de150366b2ab3ee508695853
sanish <notsansh@outlook.com>
2024-07-16 17:06:37 +0545

    useState with object

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <HookCounterTwo />
+      <HookCounterThree />
     </>
   );
 }
