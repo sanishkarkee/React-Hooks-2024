176192d3861de04d85a0b853ebd8d5784618a0df
sanish <notsansh@outlook.com>
2024-07-19 13:15:36 +0545

    conditionally render useEffects using dependencies

--- src/components/HookCounterOne.jsx
+++ src/components/HookCounterOne.jsx
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from 'react';
 
 function HookCounterOne() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
 
   useEffect(() => {
-    document.title = `you clicked ${count} times`;
-  });
-
+    console.log('useEffect is running');
+    document.title = `you clicked ${count} times.`;
+  }, [count]);
   return (
     <>
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button onClick={() => setCount((preValue) => preValue + 1)}>
         Click {count} times
       </button>
