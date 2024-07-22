753dc99129f149d97212df487204ddc912e06f74
sanish <notsansh@outlook.com>
2024-07-22 14:25:48 +0545

    Fethcing data through id only

--- src/components/DataFetching.jsx
+++ src/components/DataFetching.jsx
@@ -1,30 +1,38 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-function DataFetching() {
-  const [posts, setPosts] = useState([]); //[] leko because APi bata aaune data arrays of objects ma xa
+// Initially return bhitra ko html render bhaye paxi matra useEffect ko code run hunxa
 
-  console.log(posts);
+function DataFetching() {
+  // const [posts, setPosts] = useState([]); //[] leko because APi bata aaune data arrays of objects ma xa
+  const [post, setPost] = useState({}); //mathi array thyo yaha  { } xa because yaha id through bat single object GET garinxa so
+  const [id, setId] = useState(1);
+
+  // console.log(post);
 
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/posts')
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => {
-        console.log(res); // yo res ma chai sab name:value properties haru aauxa
-        setPosts(res.data); //tyo aako name:value properties ma chai "data" bhanne properties ma actual khojeko data haru hunxa
+        console.log(res); // yo res ma chai sab (name:value) properties haru aauxa
+        setPost(res.data); //tyo mathi aayeko (name:value) properties ma chai "data" bhanne properties ma actual khojeko data haru hunxa example : "userId": 1 - "id": 1 - "title": "sunt aut facere" - "body": "quia et suscipit nsuscipit recusandae"
         // console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [id]);
+
   return (
     <>
-      <ul>
+      <input type='text' value={id} onChange={(e) => setId(e.target.value)} />
+      <p>{post.title}</p>
+
+      {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </>
   );
 }
