b26b05bd3c46b6ff41976e726254fde16d00a627
sanish <notsansh@outlook.com>
2024-07-23 13:33:06 +0545

    :bookmark: useContext Hook using createContext() and useContext()

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import './App.css';
-import DataFetching from './components/DataFetching';
+import CompA from './components/CompA';
+
+export const NameContext = createContext();
+export const channelContext = createContext();
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState();
 
   return (
     <>
-      <DataFetching />
+      <NameContext.Provider value={'Sanish'}>
+        <channelContext.Provider value={count}>
+          <CompA />
+        </channelContext.Provider>
+      </NameContext.Provider>
     </>
   );
 }
